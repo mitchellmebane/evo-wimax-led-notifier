9c139d1f85720436cef47ba09ffbed73ba0b2eff
Mitchell Mebane <mitchellmebane@users.noreply.github.com>
2011-01-25 02:13:08 +0000

    * Add root code * Bump to v2011-01-24.1

--- src/com/mitchellmebane/android/wimaxnotifier/lib/WiMAXLED.java
+++ src/com/mitchellmebane/android/wimaxnotifier/lib/WiMAXLED.java
@@ -8,10 +8,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import android.util.Log;
+
 import com.mitchellmebane.android.wimaxnotifier.utils.Misc;
 
 
 public class WiMAXLED {
+    private static final String TAG = "WiMAX Message Notifier - WiMAXLED";
     public static final String CONTROL_DEVICE = "/sys/class/leds/wimax/brightness";
 
     public enum Mode {
@@ -62,21 +65,70 @@ public class WiMAXLED {
     }
     
     private WiMAXLED() {
+        // pure static class, no instantiation allowed 
     }
     
-    public static void setMode( int mode ) throws IOException {
-        // convert the mode to a string
-        String modeString = Integer.toString( mode );
+    
+    // TODO: Handle this return code
+    public static boolean setMode( int mode ) throws IOException {
+        Log.i( TAG, "setMode()" );
         
         File ledDevice = new File( CONTROL_DEVICE );
-        DataOutputStream ledOut = new DataOutputStream( new FileOutputStream( ledDevice ) );
         
-        ledOut.writeBytes( modeString );
-        ledOut.flush();
+        if( ledDevice.canWrite() ) {
+            // convert the mode to a string
+            String modeString = Integer.toString( mode );
+            
+            DataOutputStream ledOut = new DataOutputStream( new FileOutputStream( ledDevice ) );
+            ledOut.writeBytes( modeString );
+            Misc.closeOrLikeWhatever( ledOut );
+            
+            return true;
+        }
+        else {
+            Log.w( TAG, "Unable to write to device, trying root" );
+            return setModeAsRoot( mode );
+        }
         
-        Misc.closeOrLikeWhatever( ledOut );
     }
     
+    /**
+     * Attempts to set the mode using root privileges
+     * @param mode The integer mode to set
+     * @return <code>true</code> if root was successfully gained, <code>false</code> otherwise.
+     */
+    private static boolean setModeAsRoot( int mode ) {
+        String command = "echo " + mode + " > " + CONTROL_DEVICE + "\n";
+        
+        boolean gotRoot = false;
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec( "su" );
+            
+            DataOutputStream dout = new DataOutputStream( p.getOutputStream() );
+            dout.writeBytes( command );
+            dout.flush();
+            
+            dout.writeBytes( "exit\n" );
+            dout.flush();
+            
+            p.waitFor();
+            if( p.exitValue() == 0 ) {
+                Log.i( TAG, "Successfully got root, hope it worked..." );
+                gotRoot = true;
+            }
+        }
+        catch( Exception e ) {
+            Log.i( TAG, "Error while trying to get root", e );
+        }
+        
+        if( !gotRoot ) {
+            Log.i( TAG, "Unable to get root :(" );
+        }
+        
+        return gotRoot;
+    }
+
     public static void setMode( Mode mode ) throws IOException {
         setMode( mode.getCode() );
     }
