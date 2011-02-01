9654c6ac277dac0ed28f27894103e841e3810e5c
mitchell.mebane@gmail.com <mitchell.mebane@gmail.com@8c6671a9-d153-f097-4271-49f45f62fc6e>
2011-02-01 04:00:16 +0000

    Why did the tag pick up uncommitted changes?  Silly SVN.

--- tags/v2011-01-30.2/src/com/mitchellmebane/android/wimaxnotifier/exceptions/InvalidBroadcastException.java
+++
@@ -1,9 +0,0 @@
-package com.mitchellmebane.android.wimaxnotifier.exceptions;
-
-public class InvalidBroadcastException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidBroadcastException( String message ) {
-        super( message );
-    }    
-}
--- tags/v2011-01-30.2/src/com/mitchellmebane/android/wimaxnotifier/utils/L.java
+++
@@ -1,9 +0,0 @@
-package com.mitchellmebane.android.wimaxnotifier.utils;
-
-public class L {
-    private static String getInfo() {
-        StackTraceElement els[] = Thread.currentThread().getStackTrace();
-        
-        return null;
-    }
-}
